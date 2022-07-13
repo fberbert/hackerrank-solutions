fe81b6cca834e4de8ef64fefd67fc7c27d17a073
fabio <fberbert@gmail.com>
2022-07-13 17:17:17 -0300

    Algorithms - Diagonal Difference

---
+++ Algorithms/diagonal-diference.js
@@ -0,0 +1,13 @@
+/*
+https://www.hackerrank.com/challenges/diagonal-difference/problem
+*/
+
+function diagonalDifference(arr) {
+  let d1 = 0
+  let d2 = 0
+  for (let i=0; i<arr.length; i++) {
+    d1 += arr[i][i]
+    d2 += arr[i][arr.length -1 -i]
+  }
+  return Math.abs((d1 - d2))
+}
