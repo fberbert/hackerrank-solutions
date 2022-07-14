c54999c541b3f8c3620b1625e5f8b13dee3ce037
fabio <fberbert@gmail.com>
2022-07-13 23:03:51 -0300

    Algorithms - Time Conversion

---
+++ Algorithms/time-conversion.js
@@ -0,0 +1,12 @@
+/*
+ * https://www.hackerrank.com/challenges/time-conversion/problem
+ */
+
+const timeConversion = (s) => 
+  s.replace(/^([0-9]{2}):([0-9]{2}):([0-9]{2})([A-Z]{2})$/, (_, hh, mm, ss, daypart) => {
+    if ( daypart === 'PM' && hh != '12' ) hh = parseInt(hh, 10) + 12
+    if ( daypart === 'AM' && hh === '12' ) hh = '00'
+    return `${hh}:${mm}:${ss}`
+  })
+
+timeConversion('07:05:45PM')
