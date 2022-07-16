949dd7b63b615770427acb136ef5dbc4b26b41e1
Fábio Berbert de Paula <fberbert@gmail.com>
2022-07-16 04:48:18 -0300

    1 week - day 1 - mock test - find the median

--- 1-week-preparation-kit/day-1-mock-test-find-the-median.js
+++ 1-week-preparation-kit/day-1-mock-test-find-the-median.js
@@ -1,7 +1,1 @@
-const findMedian = (arr) => {
-  const s = [...arr].sort()
-  const mid = Math.floor(s.length/2)
-  return ( s.length % 2 === 0 ) 
-    ? s[mid -1] + s[mid] / 2
-    : s[mid]
-}
+const findMedian = (arr) => arr.sort((a, b) => a - b)[Math.floor(arr.length/2)]
