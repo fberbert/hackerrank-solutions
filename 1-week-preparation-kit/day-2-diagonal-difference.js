03a915f7a452bed3e8dedac691f1e45f758c6237
Fábio Berbert de Paula <fberbert@gmail.com>
2022-07-16 15:41:53 -0300

    1 week - day 2 - Diagonal difference

---
+++ 1-week-preparation-kit/day-2-diagonal-difference.js
@@ -0,0 +1,13 @@
+/*
+ * https://www.hackerrank.com/challenges/one-week-preparation-kit-diagonal-difference/problem
+ */
+
+const diagonalDifference = (arr) => {
+  let d1 = 0
+  let d2 = 0
+  for (let i=0; i<arr.length; i++) {
+    d1 += arr[i][i]
+    d2 += arr[i][arr.length -1 -i]
+  }
+  return Math.abs((d1 - d2))
+}
